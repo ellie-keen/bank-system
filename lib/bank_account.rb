4d3d3e97924016f6098337ed2832b97b12cb5f98
Ellie Keen <elliekeen95@gmail.com>
2018-06-18 14:35:27 +0100

    Initialise BankAccount with an empty array; Push amount into transactions array after depositing money

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,13 +1,15 @@
 class BankAccount
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def deposit(amount)
     raise 'Enter an amount more than 0' if negative?(amount)
     @balance += amount
+    submit_transaction(amount)
   end
 
   def withdraw(amount)
@@ -23,6 +25,10 @@ class BankAccount
 
   def insufficient_funds?(amount)
     amount > @balance
+  end
+
+  def submit_transaction(amount)
+    @transactions << amount
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -7,6 +7,10 @@ describe BankAccount do
     expect(bank_account.balance).to eq(0)
   end
 
+  it 'is initialized with an empty array "transactions"' do
+    expect(bank_account.transactions).to eq([])
+  end
+
   describe '#deposit' do
     it 'should add the amount to the balance' do
       expect { bank_account.deposit(100) }.to change { bank_account.balance }.by(100)
@@ -14,6 +18,11 @@ describe BankAccount do
 
     it 'should raise an error if depositing a negative number' do
       expect { bank_account.deposit(-100) }.to raise_error('Enter an amount more than 0')
+    end
+
+    it 'should add the amount to the transactions array' do
+      bank_account.deposit(100)
+      expect(bank_account.transactions).to eq([100])
     end
   end
 
@@ -27,5 +36,7 @@ describe BankAccount do
       expect { bank_account.withdraw(100) }.to raise_error('Insufficient funds')
     end
   end
+
+
 
 end
