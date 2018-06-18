92aafc951dcf93d794671833c818dff622cf3043
Ellie Keen <elliekeen95@gmail.com>
2018-06-18 12:22:01 +0100

    Raise error if amount entered is negative; Add negative? private method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,6 +6,13 @@ class BankAccount
   end
 
   def deposit(amount)
+    raise "Enter an amount more than 0" if negative?(amount)
     @balance += amount
+  end
+
+  private
+
+  def negative?(amount)
+    amount < 0
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -11,5 +11,9 @@ describe BankAccount do
     it 'should add the amount to the balance' do
       expect { bank_account.deposit(100) }.to change { bank_account.balance }.by(100)
     end
+
+    it 'should raise an error if depositing a negative number' do
+      expect { bank_account.deposit(-100) }.to raise_error("Enter an amount more than 0")
+    end
   end
 end
