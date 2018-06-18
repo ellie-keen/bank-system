007b8868ec7867be5644598a88ec02c80350f74f
Ellie Keen <elliekeen95@gmail.com>
2018-06-18 12:11:08 +0100

    Add deposit method

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,4 +6,10 @@ describe BankAccount do
   it 'is initialized with a balance of 0' do
     expect(bank_account.balance).to eq(0)
   end
+
+  describe '#deposit' do
+    it 'should add the amount to the balance' do
+      expect { bank_account.deposit(100) }.to change { bank_account.balance }.by(100)
+    end
+  end
 end
