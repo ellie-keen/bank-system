fc48e30ef038fbd8266e6ff5686860d257619ead
Ellie Keen <elliekeen95@gmail.com>
2018-06-19 18:13:16 +0100

    Print statement method

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,9 +1,9 @@
-require 'bank_account'
-
 class BankStatement
 
-  def initialize(bank_account)
-    @bank_account = bank_account
+  def print_statement(bank_account)
+    bank_account.transaction_history.each do |transaction|
+      return "Amount: #{transaction.amount}\nDate: #{transaction.date}\nBalance: #{transaction.balance}"
+    end
   end
 
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -1,11 +1,16 @@
 require 'bank_statement'
+require 'bank_account'
 
 describe BankStatement do
 
-  it 'prints the bank statement' do
-    account = BankAccount.new
-    account.deposit(100)
-    BankStatement.new(account).print_statement.to eq('Amount: ')
+  subject(:bank_statement) { BankStatement.new }
+  let(:bank_account) { BankAccount.new }
+
+  describe '#print_statement' do
+    it 'should print a bank statement of all previous transactions' do
+      bank_account.deposit(100)
+      expect(bank_statement.print_statement(bank_account)).to eq("Amount: 100\nDate: 19/06/2018\nBalance: 100")
+    end
   end
 
 end
