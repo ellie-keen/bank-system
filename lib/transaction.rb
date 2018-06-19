755f0d49df778f84283c4c414db157021a6172d3
Ellie Keen <elliekeen95@gmail.com>
2018-06-19 12:08:38 +0100

    Add Transaction class that takes an amount, balance, date and type of transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,12 @@
 class Transaction
 
-  attr_reader :amount, :date
+  attr_reader :amount, :date, :balance, :type
 
-  def initialize(amount)
+  def initialize(amount, balance, type)
     @amount = amount
     @date = Time.now.strftime("%d/%m/%Y")
+    @balance = balance
+    @type = type
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,7 @@
 require 'transaction'
 
 describe Transaction do
-  subject(:transaction) { Transaction.new(100) }
+  subject(:transaction) { Transaction.new(100, 100, 'deposit') }
 
   it 'should be initialized with an amount' do
     expect(transaction.amount).to eq(100)
@@ -9,5 +9,13 @@ describe Transaction do
 
   it 'should be initialized with a date' do
     expect(transaction.date).to eq('19/06/2018')
+  end
+
+  it 'should be initialized with a balance' do
+    expect(transaction.balance).to eq(100)
+  end
+
+  it 'should be initialized with a type' do
+    expect(transaction.type).to eq('deposit')
   end
 end
