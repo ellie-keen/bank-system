d99826a75c8908276793da442cc380a9d9e8fa8d
Ellie Keen <elliekeen95@gmail.com>
2018-06-19 21:36:55 +0100

    Refactor bank statement

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,15 +1,23 @@
 class BankStatement
 
+  HEADER = "date || credit || debit || balance\n"
+
   def print_statement(bank_account)
-    statement = "date || credit || debit || balance\n"
-    bank_account.transaction_history.each do |transaction|
-      if transaction.type == 'deposit'
-        statement += "#{transaction.date} || #{'%.2f' % transaction.amount} || || #{'%.2f' % transaction.balance}\n"
-      else
-        statement += "#{transaction.date} || || #{'%.2f' % transaction.amount} || #{'%.2f' % transaction.balance}\n"
-      end
+    statement = HEADER
+    bank_account.transactions.each do |transaction|
+      date = "#{transaction.date} ||"
+      credit_or_debit = check_type(transaction)
+      balance = " #{'%.2f' % transaction.balance}\n"
+      statement += date + credit_or_debit + balance
     end
     statement
+  end
+
+  private
+
+  def check_type(transaction)
+    transaction.type == 'withdraw' ? " || #{'%.2f' % transaction.amount} ||" :
+      " #{'%.2f' % transaction.amount} || ||"
   end
 
 end
